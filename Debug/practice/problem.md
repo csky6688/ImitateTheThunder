0e458b02eac572b8ab869b7b71e7349f1b404b21
yszs <362409100@qq.com>
2017-10-13 00:17:51 +0800

    在treenode中使用option控件，添加listdmemo

---
+++ Debug/practice/problem.md
@@ -0,0 +1,26 @@
+## Q1:TreeNode 消息响应
+
+### A1:
+
+
+
+## Q2:TreeNode字体设置
+
+### A2:只能通过代码设置
+
+
+
+## Q3:TileLayout itemsize属性设置，高度无效
+
+### A3:
+
+
+
+## Q4:TileLayout 自动调整布局错误
+
+### A4:参考 [CSDN](http://blog.csdn.net/whuavon/article/details/38389961)，是因为padding没有考虑
+
+
+
+
+
--- Practice/DuiMainWnd.cpp
+++ Practice/DuiMainWnd.cpp
@@ -34,19 +34,16 @@ void CDuiMainWnd::InitWindow()
 {
 	m_PaintManager.RemoveAllFonts(true);
 	m_PaintManager.AddFont(0, L"Microsoft YaHei UI", 20, false, false, false, true);
-	CLabelUI* pLabel = static_cast<CLabelUI*>(m_PaintManager.FindControl(_T("btndemotext")));
-	if (pLabel != NULL)
-	{
-		pLabel->SetFont(0);
-	}
-	pLabel = static_cast<CLabelUI*>(m_PaintManager.FindControl(_T("tiledemotext")));
-	if (pLabel != NULL)
-	{
-		pLabel->SetFont(0);
-	}
+	COptionUI* pOpt = static_cast<COptionUI*>(m_PaintManager.FindControl(_T("optButton")));
+	if (pOpt != NULL)
+		pOpt->SetFont(0);
+	pOpt = static_cast<COptionUI*>(m_PaintManager.FindControl(_T("optTile")));
+	if (pOpt != NULL)
+		pOpt->SetFont(0);
+	pOpt = static_cast<COptionUI*>(m_PaintManager.FindControl(_T("optList")));
+	if (pOpt != NULL)
+		pOpt->SetFont(0);
 
-	CTreeNodeUI* pTab = static_cast<CTreeNodeUI*>(m_PaintManager.FindControl(_T("tiledemo")));
-	pTab->OnNotify += MakeDelegate(this,&TreeNodeItemEvent);
 }
 
 CControlUI* CDuiMainWnd::CreateControl(LPCTSTR pstrClassName)
@@ -61,11 +58,6 @@ CControlUI* CDuiMainWnd::CreateControl(LPCTSTR pstrClassName)
 	{
 		pUI = builder.Create(L"rightPane.xml");
 	}
-	/*else if (_tcscmp(pstrClassName, L"MyColorSkin") == 0)
-	{
-	CColorSkinUI* color = new CColorSkinUI();
-	pUI = color;
-	}*/
 	return pUI;
 
 	//return NULL;
@@ -79,21 +71,19 @@ void CDuiMainWnd::TreeNodeItemEvent()
 
 void CDuiMainWnd::Notify(TNotifyUI& msg)
 {
-	if (msg.sType = DUI_MSGTYPE_ITEMACTIVATE)
+	if (msg.sType == L"selectchanged")
 	{
-		if (msg.pSender->GetName() == L"tiledemo")
-		{
-			MessageBox(NULL, L"1", L"123", NULL);
+		CDuiString strName = msg.pSender->GetName();
+		CTabLayoutUI* pUI = (CTabLayoutUI*)m_PaintManager.FindControl(L"tab");
 
-			CTabLayoutUI* pTab = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("tab")));
-			pTab->SelectItem(1);
-		}
-		else
-		{
-			CTabLayoutUI* pTab = static_cast<CTabLayoutUI*>(m_PaintManager.FindControl(_T("tab")));
-			pTab->SelectItem(0);
-		}
+		if (strName == L"optButton")
+			pUI->SelectItem(0);
+		else if (strName == L"optTile")
+			pUI->SelectItem(1);
+		else if (strName == L"optList")
+			pUI->SelectItem(2);
 	}
+
 
 	__super::Notify(msg);
 }
